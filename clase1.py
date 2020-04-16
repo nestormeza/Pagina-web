32a5e0020bbdfa6f665834313c312f0487784ebd
NestorRosas <nestor.mmeza97@gmail.com>
2020-04-15 21:19:44 -0500

    clase 2

---
+++ clase1.py
@@ -0,0 +1,6 @@
+#hola esto es un cometario
+print ("Hola mundo soy Nestor  alumno del curso python master")
+#cometario multilina
+"""
+amsjsjsjs
+"""
---
+++ clase2-variables.py
@@ -0,0 +1,33 @@
+# una variable almacena un valor
+
+texto = 'Master del curso de python'
+texto2 = 'con Nestor Rosas'
+num = 10
+decimal = 35.6
+lista = [2,3,4,5]
+tupla = ('nombre','Nestor') #las duplas no cambias
+diccionario = {
+    'Nombre' : 'Nestor',
+    'Apellidos' : 'Rosas'
+}
+rango = range(9) # rango de numeros
+boolean = False
+
+#imprimir datos
+print(texto)
+print(texto2)
+print(num)
+print(decimal)
+print(lista)
+print(tupla)
+print(diccionario)
+print(rango)
+print(boolean)
+
+#concatenar
+print(texto+' '+texto2)
+print('hola {} + {}'.format(texto,texto2))
+# print(f'{texto}')
+
+#saber el tipo de datos
+print(type(texto))
